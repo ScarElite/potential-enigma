ab57c21542c4d5f8b726f8dddbeeaf44de78e4eb
Mitchell Armstrong <mra0211@gmail.com>
2022-09-25 23:56:10 -0400

    Created functions that return the license section and the license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,11 +17,30 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "The MIT License":
+      return `[MIT License](https://www.mit.edu/~amini/LICENSE.md)`;
+    case "Apache License":
+      return `[Apache License](https://www.apache.org/licenses/LICENSE-2.0)`;
+    case "GNU License":
+      return `[GNU License](https://www.gnu.org/licenses/gpl-3.0.en.html)`;
+    case "UNLICENSED":
+      return ``;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "N/A") {
+    return "";
+  } else {
+    return `This project uses the following license 
+    ${renderLicenseLink(license)}
+    `;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
